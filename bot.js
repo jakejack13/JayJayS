3f5f25896106598f42a869aabfd7560393c21d3f
Jacob Kerr <jck268@cornell.edu>
2021-11-05 11:12:06 -0400

    Added hello comamnd and slash command regsitering

---
+++ bot.js
@@ -0,0 +1,20 @@
+//@ts-check
+
+require('dotenv').config()
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'hello') {
+    await interaction.reply(`Hello, <@${interaction.member.user.id}>`);
+  }
+});
+
+client.login(process.env.TOKEN);
